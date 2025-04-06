0e5ab1d1b423efaaf2b93a9b2cc19e701cdf1f70
Typezera <willianbardela2131@hotmail.com>
2025-04-06 19:16:01 -0300

    restaura classe Usuário

--- src/main/java/com/LembreteMedicamento/lembretemedicamento/model/Usuario.java
+++ src/main/java/com/LembreteMedicamento/lembretemedicamento/model/Usuario.java
@@ -1,4 +1,21 @@
 package com.LembreteMedicamento.lembretemedicamento.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    @Column(unique = true)
+    String email;
+    String senha;
 }
