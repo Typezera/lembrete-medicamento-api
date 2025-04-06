784618b2f5eb7e3afe872d00d227b1baca5b3c15
Typezera <willianbardela2131@hotmail.com>
2025-04-06 17:45:01 -0300

    projeto recriado com spring Initializr

---
+++ src/main/java/com/LembreteMedicamento/lembretemedicamento/LembretemedicamentoApplication.java
@@ -0,0 +1,13 @@
+package com.LembreteMedicamento.lembretemedicamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembretemedicamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembretemedicamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LembreteMedicamento/lembretemedicamento/LembretemedicamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LembreteMedicamento.lembretemedicamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembretemedicamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
