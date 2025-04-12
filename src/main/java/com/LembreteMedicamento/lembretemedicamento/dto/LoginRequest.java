2bf2df45fb8168be8df06942b7b584874c37f5f7
Typezera <willianbardela2131@hotmail.com>
2025-04-12 13:23:36 -0300

    deletando código refatoração de código o código

--- src/main/java/com/LembreteMedicamento/lembretemedicamento/dto/LoginRequest.java
+++ src/main/java/com/LembreteMedicamento/lembretemedicamento/dto/LoginRequest.java
@@ -8,15 +8,8 @@ public class LoginRequest {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getSenha() {
         return senha;
     }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
